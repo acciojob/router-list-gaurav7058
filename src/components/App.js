794241e5d08345ca5f3c1dff56744ca48a8a707a
Create_first_repo <gauravchoundiye123@gmail.com>
2024-10-12 17:35:33 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter , Routes, Route, Switch } from 'react-router-dom';
+import Links from "./Links";
+import Item1 from "./Item1";
+import Item2 from "./Item2";
+import Item3 from "./Item3";
+import Home from "./Home";
 
 const App = () => {
   return (
+    <BrowserRouter>
+   
     <div>
-        {/* Do not remove the main div */}
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/item1/:userId" element={<Item1></Item1>}></Route>
+          <Route path="/item2/:userId" element={<Item2></Item2>}></Route>
+          <Route path="/item3/:userId" element={<Item3></Item3>}></Route>
+        </Routes>
     </div>
+    <Links></Links>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Links.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link} from "react-router-dom"
+export default function Links() {
+  return (
+    <div>
+      <Link to="/item/1">Item-1</Link>
+      <Link to="/item/2">Item-2</Link>
+      <Link to="/item/3">Item-3</Link>
+    </div>
+  )
+}
